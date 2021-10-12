6944d201fb98dc05b015780dfe5cf7a1f0637e05
dwiyaazhari <91622068+dwiyaazhari@users.noreply.github.com>
2021-10-12 23:25:54 +0700

    Add files via upload

---
+++ BiodataSaya.java
@@ -0,0 +1,25 @@
+public class BiodataSaya{
+    public static void main(String[] args){
+        //first name, last name, age, address, hobi, phone number, makanan kesukaan, tinggi
+        String firstName, lastName, address, hobi, makananKesukaan, phoneNumber;
+        int age;
+        double tinggi;
+
+        firstName = "Dwiya";
+        lastName = "Azhari";
+        address = "Swis,sekitar wilayah sunggal";
+        hobi = "main ps";
+        makananKesukaan = "bakso";
+        phoneNumber = "08123456789";
+        age = 25;
+        tinggi = 190.3;
+
+        System.out.println("Name : " + firstName + lastName);
+        System.out.println("address : " + address);
+        System.out.println("hobi : " + hobi);
+        System.out.println("makanan kesukaan : " + makananKesukaan);
+        System.out.println("phone number : " + phoneNumber);
+        System.out.println("age : " + age);
+        System.out.println("tinggi : " + tinggi);
+    }   
+}
